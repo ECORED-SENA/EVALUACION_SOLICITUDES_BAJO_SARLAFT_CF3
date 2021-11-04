b4dcaca2e31513fb2ab599fd7ff3e1a11f7f3655
Andrea Botello <andrea.botellor@misena.edu.co>
2021-11-03 21:13:08 -0500

    updated

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -19,7 +19,7 @@
     .row.justify-content-center.tarjeta.fondo-video.mb-4(data-aos="fade-down") 
       .col-md-10.py-5
         .video
-          iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+          iframe(width="560" height="315" src="https://www.youtube.com/embed/0NG6EHLnHAM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
     p(data-aos="fade") Para la elaboración de este componente, se abordaron varios autores conocidos en gestión <b>SARLAFT</b>, de quienes se han citado y referenciado conceptos y ejemplos para los fines educativos de esta materia, en el entendido de que el conocimiento es social y, por lo tanto, es para ser usado por quienes necesitan adquirirlo. Se espera que este documento sea útil para todos aquellos, aprendices y lectores en general, que estén interesados en acercarse a asuntos básicos de las solicitudes financieras bajo la normatividad <b>SARLAFT</b>.
 
